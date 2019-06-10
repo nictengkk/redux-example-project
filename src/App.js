99b01d25d3a8579f042fb3ad36c4b70e3a61504b
nictengkk <nictengkk@gmail.com>
2019-06-11 01:03:27 +0800

    install redux modules and update app.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Posts from "./components/Posts";
+import Postform from "./components/Postform";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        Learn React
-      </header>
+      <Postform />
       <Posts />
     </div>
   );
